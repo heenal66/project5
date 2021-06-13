10b9be8d367185859fbdfa7c8ffb8fb3e268be4a
heenal66 <85824805+heenal66@users.noreply.github.com>
2021-06-13 14:57:48 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+var filed = document.getElementById('btn').onclick = function () {
+    alert('This booking is filed');
+};
+var about = document.getElementById('about').onclick = function () {
+    alert('scroll down for all the details');
+}
+var read = document.getElementById('read').onclick = function () {
+    document.write('Hello Peeps!');
+}
